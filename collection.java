233f8f3bffd57a4c03ac680f7d665519452ff7b8
jake.0208 <jake.0208@kakaocorp.com>
2022-01-16 11:41:14 +0900

    collection test

---
+++ collection.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class collection{
+	public static void main(String[] args){
+		List<Integer> list = new ArrayList<>();
+		list.add(10);
+		System.out.println(list);
+	}
+}
